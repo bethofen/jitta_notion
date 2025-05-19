e69af0f24edc88b7c22d0c52ede35e7007c09940
youtnp00 <thanapon_kh65@live.rmutl.ac.th>
2025-05-19 23:50:26 +0700

    add backtest

--- module.py
+++ module.py
@@ -20,3 +20,59 @@ def get_driver():
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     driver = webdriver.Chrome(options=options)
     return driver
+
+class basic_back_test:
+    def __init__(self):
+        self.reset()
+    def run_backtest(self):
+       pass    
+    def close_order(self,close_price):
+        if self.current_position == "BUY":
+            pre_profit = close_price - self.open_position_price
+            self.list_trade.append(pre_profit)                                                                                                                                                                         
+            self.percen_trade.append(pre_profit / self.open_position_price * 100)
+        elif self.current_position == "SELL":
+            pre_profit = self.open_position_price - close_price
+            self.list_trade.append(pre_profit)  
+            self.percen_trade.append(pre_profit / self.open_position_price * 100)
+        else:
+            raise ValueError("nope")
+        
+        self.open_position_price = None
+        self.switch_free +=1  
+        self.current_position = None
+        data = self.data.iloc[self.i]
+        self.detail_trade["close_date"] = data["datetime"]
+        self.detail_trade["close_price"] = close_price
+        self.detail_all_trade.append(self.detail_trade)
+        self.detail_trade = {}
+    def get_order(self,position,open_price):
+        self.current_position = position
+        self.open_position_price = open_price
+        data = self.data.iloc[self.i]
+        self.detail_trade["position"] = position
+        self.detail_trade["open_date"] = data["datetime"]
+        self.detail_trade["open_price"] = open_price
+  
+    def reset(self):
+        self.current_position = None
+        self.open_position_price = None
+        self.trailing_stop = None
+        self.list_trade = []
+        self.switch_free = 0
+        self.multiply_stop = 2
+        self.percen_trade = []
+        self.detail_trade = {}
+        self.detail_all_trade = []
+    def load_data(self,data):
+        self.data = data
+    def find_avg_order(self):
+        all_position = ['high','low','close','open']
+        all_position = [self.data.iloc[self.i][po] for po in all_position]
+        return sum(all_position) / len(all_position)    
+    def get_history(self):
+        return self.list_trade
+    def get_percen(self):
+        return self.percen_trade
+    def get_detailAllTrade(self):
+        return self.detail_all_trade
